1622fd6814bae248692b0080c7ecdd50cce1f29e
Sventimir <svantimir@gmail.com>
2024-10-09 11:44:48 +0200

    Update submit_intent for Solana.

--- sdk/src/solana/client.rs
+++ sdk/src/solana/client.rs
@@ -2,13 +2,16 @@ use crate::solana::{ChainError, Cluster};
 use crate::Chain;
 use anchor_client::Cluster as SolanaCluster;
 use anchor_lang::prelude::Pubkey;
+use anchor_spl::associated_token::get_associated_token_address;
 use async_trait::async_trait;
 use mantis_common::UserIntent;
 use solana_client::rpc_client::RpcClient;
 use solana_client::rpc_config::RpcSendTransactionConfig;
 use solana_sdk::commitment_config::CommitmentConfig;
 use solana_sdk::signature::{Keypair, Signer};
+use rand::{distributions::Alphanumeric, Rng};
 use std::sync::Arc;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 pub(crate) struct SolanaClient {
     rpc_client: RpcClient,
@@ -72,15 +75,53 @@ impl Chain for SolanaClient {
             ChainError::StoreIntentError(format!("Failed to get program instance: {}", e))
         })?;
 
-        let intent_id = b"123"; // TODO
-        let _intent_state = Pubkey::find_program_address(&[b"intent", intent_id], &program.id()).0;
+        let intent_id: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(6)
+            .map(char::from)
+            .collect();
+        let intent_state = Pubkey::find_program_address(&[b"intent", intent_id.as_bytes()], &program.id()).0;
+
+        let current_timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
+        let new_intent = bridge_escrow::IntentPayload {
+            intent_id: intent_id.clone(),
+            user_in: auctioneer.pubkey(), // Must match the ctx.accounts.user key in the contract
+            user_out: auctioneer.pubkey().to_string(),
+            token_in: intent.token_in.parse()?,
+            amount_in: intent.amount_in.parse()
+                .map_err(|e: std::num::ParseIntError| ChainError::Other(e.to_string()))?,
+            token_out: intent.token_out.clone(),
+            amount_out: intent.amount_out.to_string(), // Amount out as a string
+            timeout_timestamp_in_sec: current_timestamp + 10000, // Arbitrary timeout
+            single_domain: true,
+        };
+
+        let auctioneer_state = Pubkey::find_program_address(&[b"auctioneer"], &bridge_escrow::ID).0;
+        let user_token_account = 
+            get_associated_token_address(&auctioneer.pubkey(), &new_intent.token_in);
+        let escrow_token_account =
+            get_associated_token_address(&auctioneer_state, &new_intent.token_in);
 
         let result = program
             .request()
-            .accounts(mantis_escrow_program::accounts::Initialize {})
-            .args(mantis_escrow_program::instruction::EscrowAndStoreIntent {
-                _amount: 0,
-                _new_intent: intent,
+            .accounts(bridge_escrow::accounts::EscrowAndStoreIntent {
+                user: auctioneer.pubkey(),
+                user_token_account,
+                auctioneer_state,
+                token_mint: new_intent.token_in,
+                escrow_token_account,
+                intent: intent_state,
+                token_program: anchor_spl::token::ID,
+                associated_token_program: anchor_spl::associated_token::ID,
+                system_program: anchor_lang::system_program::ID,
+            })
+            .args(bridge_escrow::instruction::EscrowAndStoreIntent {
+                amount: new_intent.amount_in,
+                new_intent,
             })
             .payer(auctioneer.clone())
             .signer(auctioneer.as_ref())
